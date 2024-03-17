5ef127c932756ee41e265c44a49c902da2de86d1
s3a6m9 <74426924+s3a6m9@users.noreply.github.com>
2024-03-17 21:14:21 +0000

    setup.py fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'encrypt=encrypter.main:main',
+            'encrypter=encrypter:main',
         ],
     },
 )
